8b307c648fe4a1f06ef73adeede1fc51e450810c
Mike <mikkrueger@team.mobile.de>
2020-02-12 14:54:26 +0100

    fix test

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -1,7 +1,7 @@
 describe('Homepage is available', function () {
     it('check if user can visit my homepage', function () {
         cy.visit('https://www.kruegermike.de')
-        cy.get('body > header > div > div > h1').contains('MADMIKE')
+        cy.get('body > header > div > div > h1').contains('Madmike')
         cy.wait(1500)
     
     })
