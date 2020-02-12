66becb10353b50f57a2399e4d4b7c282c121ccef
Mike <mikkrueger@team.mobile.de>
2020-02-12 13:43:39 +0100

    first commit to my own github space

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() { 
+//     var gradients = [
+//         'linear-gradient(to right, #48c6ef, #6f86d6)'
+//     ];
+
+//     var randomIndex = Math.floor(Math.random() * gradients.length);
+//     $(".background-change").css("background", gradients[randomIndex]);
+// });
