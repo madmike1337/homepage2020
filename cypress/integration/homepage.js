1171046ad10ad6723b121d6437e1328d089423d9
Mike <mikkrueger@team.mobile.de>
2020-02-13 10:05:04 +0100

    try things out

--- cypress/integration/homepage.js
+++ cypress/integration/homepage.js
@@ -2,6 +2,5 @@ describe('Homepage is available', function () {
     it('check if homepage has correct url', function () {
         cy.visit('https://www.kruegermike.de')
         cy.url().should('contain', 'https://www.kruegermike.de');
-
     })
 })
